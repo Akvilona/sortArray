fcb0860e450ff8f874a23addbb55541e58ad3530
Andrey Antonov <akvilona@list.ru>
2024-02-08 11:59:21 +0300

    firs commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{64, 42, 73, 41, 31, 53, 16, 24, 57, 42, 74, 55, 36};
+
+        printArray(array);
+
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            isSorted = true;
+            /* Прямой проход */
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted == false) {
+                printArray(array);
+            }
+
+            /* уменьшаем количество проходов за счет обратного прохода
+            * поэтому этот кусочек не обязателен и он закомментирован */
+            /*for (int i = array.length - 1; i > 0; i--) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted == false) {
+                printArray(array);
+            }*/
+        }
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(arr[i]);
+        }
+        System.out.println("]");
+    }
+}
